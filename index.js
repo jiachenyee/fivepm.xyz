363e89c742851cddb85ed9fd34028984f6eec128
Xenia Fiorenza <xenia.anong@gmail.com>
2022-02-17 16:26:31 +0800

    fix typo

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var fivePMText = document.getElementById("5pm");
 setTimeout(function () {
 	var date = new Date()
 
-	fivePMTex3.innerText = date.getHours() == 17 ? "yes" : "no"
+	fivePMText.innerText = date.getHours() == 17 ? "yes" : "no"
 }, 30000)

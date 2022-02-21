73166580100d01a98aa60c7c76c89e3c69784b3c
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-02-21 23:17:10 +0800

    hide AR button if user is not on iOS device

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var fivePMText = document.getElementById("5pm");
 var arBadge = document.getElementById("arbadge");
 var now = new Date();
+
 var fivePM = new Date();
+var sixPM = new Date();
+
 var focusCount = 0;
 
 reload();
@@ -15,17 +18,29 @@ function reload() {
         fivePM.setHours(17, 0, 0, 0);
         fivePM.setDate(now.getDate() + 1);
     }
+	
+    if (now.getHours() < 18) {
+        sixPM.setHours(18, 0, 0, 0);
+    } else {
+        sixPM.setHours(18, 0, 0, 0);
+        sixPM.setDate(now.getDate() + 1);
+    }
 
     var timeToFivePM = fivePM - now;
-    var timeToSixPM = new Date();
-	
-	timeToSixPM.setHours(18, 0, 0, 0);
+	var timeToSixPM = sixPM - now;
 
     setTimeout(reload, timeToFivePM); //updates at 5pm
     setTimeout(roll, timeToSixPM); //updates at 6pm
-
+	
 	fivePMText.innerText = now.getHours() == 17 ? "yes" : "no";
-    arBadge.href = now.getHours() == 17 ? "assets/yes.reality" : "assets/no.reality";
+	
+	console.log(navigator.userAgent)
+	if(/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+	    arBadge.href = now.getHours() == 17 ? "assets/yes.reality" : "assets/no.reality";	 	
+	} else {
+		arBadge.style.display = "none"
+	}
+
 }
 
 //#-hidden-code
